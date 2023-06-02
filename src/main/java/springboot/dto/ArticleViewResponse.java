beedb3191b885747156058e7d546725329ce1df5
CDBchan <kdjin1215@naver.com>
2023-06-02 19:07:38 +0900

    feat: chapter6

---
+++ src/main/java/springboot/dto/ArticleViewResponse.java
@@ -0,0 +1,24 @@
+package springboot.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import springboot.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+  private Long id;
+  private String title;
+  private String content;
+  private LocalDateTime createdAt;
+
+  public ArticleViewResponse(Article article) {
+    this.id = article.getId();
+    this.title = article.getTitle();
+    this.content = article.getContent();
+    this.createdAt = article.getCreatedAt();
+  }
+}
